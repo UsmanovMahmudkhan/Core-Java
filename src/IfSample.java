ae5e6d9feccba59adf46d577b55f2cabc0dfd4b0
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2025-06-29 19:52:34 +0900

    Update IfSample.java

--- src/IfSample.java
+++ src/IfSample.java
@@ -1,4 +1,16 @@
 package PACKAGE_NAME;
 
 public class IfSample {
+    public static void main(String[] args) {
+        int x,y;
+        x=10;
+        y=20;
+        if(x<y) System.out.println("X is less than Y");
+        x=x*2;
+        if(x==y) System.out.println("X is equal to Y");
+        x=x*2;
+        if(x>y) System.out.println("X is grater than Y");
+        x=x*2;
+        if(x==y) System.out.println("X and y are the same");
+    }
 }
