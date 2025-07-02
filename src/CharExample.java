d43e84e644170d8fcf5ef04956a13f8432cfea33
mahmudkhon usmonov <mahmudkhonusmonov@mahmudkhons-macbook-air.local>
2025-07-02 14:08:24 +0900

    Add CharExample.java

---
+++ src/CharExample.java
@@ -0,0 +1,10 @@
+public class CharExample {
+    public static void main(String[] args) {
+        char cha1, char2;
+        cha1=88;
+        char2='x';
+        System.out.println("char1: "+cha1+"\n"+"char2: "+char2);
+
+    }
+
+}
