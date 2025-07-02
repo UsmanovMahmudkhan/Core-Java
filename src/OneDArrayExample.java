e7c4c3e718191a8f38275219308a76a86c771c04
mahmudkhonusmonov <usmanovmahmudkhan@gmail.com>
2025-07-02 17:24:32 +0900

    Merge remote-tracking branch 'origin/main'

---
+++ src/OneDArrayExample.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class OneDArrayExample {
+    public static void main(String[] args) {
+        /** First way of creating one dimentional array **/
+
+        /*      int[] months=new int[12];
+        months[0]=31; //jan
+        months[1]=28; //fev
+        months[2]=30; //mar
+        months[3]=31; //apr
+        months[4]=30; //may
+        months[5]=31; //jun
+        months[6]=30; //july
+        months[7]=31; //avg
+        months[8]=30; // sep
+        months[9]=31; // oct
+        months[10]=30; // nov
+        months[11]=31; // dec
+
+        System.out.println("ARPIL days: "+months[3]);
+
+*/
+
+        /** Second way of creating one dimentional array **/
+
+ /*    int[] months={31,28,30,31,30,31,30,31,30,31,30,31};
+     System.out.println("ARPIL days: "+months[3]);
+
+
+  */
+        /** Third way of creating one dimentional array,using Scanner **/
+
+     /*   Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of array: ");
+        int size=sc.nextInt();
+        int[] myArr=new int[size];
+        for(int i=0;i<size; i++){
+            System.out.println("Enter "+(i+1)+"th number to insert: ");
+            int myData=sc.nextInt();
+            myArr[i]=myData;
+        }
+
+        System.out.println(myArr[3]);*/
+
+    }
+
+
+
+
+
+
+}
