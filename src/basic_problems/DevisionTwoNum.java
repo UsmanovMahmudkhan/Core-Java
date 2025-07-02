2dd1afb66082ae2777a94d65a55c418388bb1853
mahmudkhonusmonov <usmanovmahmudkhan@gmail.com>
2025-07-02 19:11:58 +0900

    Merge remote-tracking branch 'origin/main'

---
+++ src/basic_problems/DevisionTwoNum.java
@@ -0,0 +1,31 @@
+package basic_problems;
+
+import java.util.Scanner;
+
+public class DevisionTwoNum {
+    /**
+     * Write a Java program to divide two numbers and print them on the screen.
+     * Test Data :
+     * 50/3
+     * Expected Output :
+     * 16
+     */
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        float num1= sc.nextFloat();
+        System.out.println("Enter second number: " );
+        float num2= sc.nextFloat();
+        if(num2==0){
+            throw new RuntimeException("cant devide number!!!" +
+                    "please enter valid number");
+        }
+        else{
+            float result=num1/num2;
+            System.out.println("Result: "+result);
+        }
+    }
+
+
+}
