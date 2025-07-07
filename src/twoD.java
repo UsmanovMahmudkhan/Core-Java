f7f187b70bb2704850bbf41f1436f897bf929b7a
mahmudkhonusmonov <usmanovmahmudkhan@gmail.com>
2025-07-07 12:34:18 +0900

    Merge remote-tracking branch 'origin/main'

---
+++ src/twoD.java
@@ -0,0 +1,16 @@
+public class twoD {
+    public static void main(String[] args) {
+        int [][] twoD=new int[3][5];
+        for(int i=0;i<3;i++){
+            for(int j=0;j<5;j++){
+                twoD[i][j]=i+j;
+            }
+        }
+        for(int i=0;i<3;i++){
+            for(int j=0;j<5;j++){
+                System.out.print(twoD[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
