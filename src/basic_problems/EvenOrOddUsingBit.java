8e6d0e2fd770787c1ac2457cd85f4463617ebd52
mahmudkhonusmonov <usmanovmahmudkhan@gmail.com>
2025-07-09 00:38:10 +0900

    Merge remote-tracking branch 'origin/main'

--- src/basic_problems/EvenOrOddUsingBit.java
+++ src/basic_problems/EvenOrOddUsingBit.java
@@ -9,5 +9,30 @@ public class EvenOrOddUsingBit {
         else{
             System.out.println("Odd");
         }
+
+                int iterations = 1_000_000_000;
+                long bitwiseSum = 0;
+                long moduloSum = 0;
+
+                long start1 = System.nanoTime();
+                for (int i = 0; i < iterations; i++) {
+                    bitwiseSum += (i & 1);
+                }
+                long end1 = System.nanoTime();
+                System.out.println("Bitwise & Time: " + (end1 - start1) + " ns");
+
+                long start2 = System.nanoTime();
+                for (int i = 0; i < iterations; i++) {
+                    moduloSum += (i % 2);
+                }
+                long end2 = System.nanoTime();
+                System.out.println("Modulo % Time: " + (end2 - start2) + " ns");
+
+                if (bitwiseSum == moduloSum) System.out.println("Result Match ");
     }
 }
+
+
+
+
+
