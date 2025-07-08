8ce92cc39f9036ab19cbff7caa8470bc4a130ab6
mahmudkhonusmonov <usmanovmahmudkhan@gmail.com>
2025-07-08 12:47:58 +0900

    Merge remote-tracking branch 'origin/main'

---
+++ src/basic_problems/SpecificValue.java
@@ -0,0 +1,24 @@
+package basic_problems;
+
+public class SpecificValue {
+    /**
+     * Write a Java program to test if an array contains a specific value.
+     *
+     * @return
+     */
+    public static void main(String[] args) {
+        int target=2;
+        boolean value=false;
+        int nums[]={3,4,5,6,7,8,9,9,2};
+        for(int n=0;n<nums.length;n++) {
+            if (target == nums[n]) {
+                value = true;
+            }
+
+        }
+        System.out.println(value);
+
+
+    }
+    }
+
