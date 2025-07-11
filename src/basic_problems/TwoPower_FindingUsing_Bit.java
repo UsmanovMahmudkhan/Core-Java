13d70b3a0515aac7c7c5d5dd48cd67519d451547
mahmudkhonusmonov <usmanovmahmudkhan@gmail.com>
2025-07-11 17:02:27 +0900

    Merge remote-tracking branch 'origin/main'

---
+++ src/basic_problems/TwoPower_FindingUsing_Bit.java
@@ -0,0 +1,22 @@
+package basic_problems;
+
+import java.util.Scanner;
+
+public class TwoPower_FindingUsing_Bit {
+    /**
+     * Return true if n is a power of two.
+     * Example:
+     * Input: n = 16
+     * Output: true
+     */
+    public static void main(String[] args) {
+        boolean powerOfTwo=false;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num=sc.nextInt();
+        if((num &(num-1))==0 && num>0){
+            powerOfTwo=true;
+        }
+        System.out.println(powerOfTwo);
+    }
+}
