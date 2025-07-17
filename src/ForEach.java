8c4ab4186428e3d976a29cc8f9a597a9ad8f3c8b
mahmudkhonusmonov <usmanovmahmudkhan@gmail.com>
2025-07-17 15:13:49 +0900

    add basic forLoop exercise using 3 loops

---
+++ src/ForEach.java
@@ -0,0 +1,13 @@
+public class ForEach {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            for(int j=0;j<4;j++) {
+                for (int g = 0; g < 2; g++) {
+                    System.out.print(i + "" + j+""+g+"\t");
+
+                }
+            }
+            System.out.println();
+        }
+    }
+}
