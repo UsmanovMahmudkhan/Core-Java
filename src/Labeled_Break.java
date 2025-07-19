864572100460e1cfc9846a5d5c48d14a08e4bf79
mahmudkhonusmonov <usmanovmahmudkhan@gmail.com>
2025-07-20 08:19:54 +0900

    added basic labeled break statement exercise using  loops

---
+++ src/Labeled_Break.java
@@ -0,0 +1,12 @@
+public class Labeled_Break {
+    public static void main(String[] args) {
+        label: for(int i=0;i<3;i++){
+            for(int j=0;j<4;j++){
+                if(j==1) break label;
+                System.out.print("Hello");
+
+            }
+            System.out.println();
+        }
+    }
+}
