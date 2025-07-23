933f1082617824258f17f398787002bbd082c467
mahmudkhonusmonov <usmanovmahmudkhan@gmail.com>
2025-07-23 17:26:27 +0900

    working with stack

---
+++ src/AStack.java
@@ -0,0 +1,44 @@
+public class AStack {
+    public static void main(String[] args) {
+
+        Stack stc=new Stack();
+        for(int i=0; i<10;i++){
+            stc.push(i*2);
+        }
+        for(int i=0; i<10;i++) {
+            System.out.println(stc.pup());
+        }
+
+    }
+}
+
+class Stack{
+    int stack[]=new int[10];
+    int item;
+
+    public Stack(){
+        item=-1;
+    }
+
+     void push(int input){
+        if(item==9){
+            System.out.println("It is overflowed");
+        }
+        else{
+            stack[++item]=input;
+        }
+    }
+
+    int pup(){
+        if(item<0){
+            System.out.println("underflow");
+            return 0;
+        }
+        else {
+            return stack[item--];
+        }
+    }
+
+
+
+}
