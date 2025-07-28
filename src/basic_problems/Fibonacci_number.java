24d37d8adf4b58094bb7acaa0fdee15e56a46be4
mahmudkhonusmonov <usmanovmahmudkhan@gmail.com>
2025-07-28 19:09:16 +0900

    modifying fibonacci numbers

--- src/basic_problems/Fibonacci_number.java
+++ src/basic_problems/Fibonacci_number.java
@@ -2,21 +2,22 @@ package basic_problems;
 
 /**
  *  Fibonacci number
- *  0 1 1
- *    1 1 2
- *      1 2 3
- *        2 3 5
+ *  (1) 0 1  1
+ *  (2) 1 1  2
+ *  (3) 1 2  3
+ *  (4) 2 3  5
  *
  */
 public class Fibonacci_number {
     public static void main(String[] args) {
 
-        System.out.println(fib(4));
+         System.out.println("answer "+fib(4));
     }
 
     static int fib(int n){
         if(n == 0) return 0;
         if(n == 1) return 1;
+        System.out.println(n);
         return fib(n-1)+fib(n-2);
     }
 
