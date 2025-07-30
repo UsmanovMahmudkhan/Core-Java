e0652f037239632bc35a0eda0163b087de3139bd
mahmudkhonusmonov <usmanovmahmudkhan@gmail.com>
2025-07-30 19:39:21 +0900

    Added Static exercisesee

---
+++ src/Static.java
@@ -0,0 +1,14 @@
+public class Static {
+    static int a=12;
+    static int b;
+
+    static void callme(int some){
+        b=some;
+        System.out.println(b);
+    }
+    public static void main(String[] args) {
+        System.out.println(a);
+        callme(19);
+
+    }
+}
