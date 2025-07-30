1bd05cfc25b7ecec1b79d64d4a3b0839594b235b
mahmudkhonusmonov <usmanovmahmudkhan@gmail.com>
2025-07-30 23:58:51 +0900

    Added final keyboard

---
+++ src/Final.java
@@ -0,0 +1,17 @@
+public class Final {
+
+    public static void main(String[] args) {
+        final int AGE=12;
+        final String NAME="Alex";
+
+        /** so when final keyboard is used,
+         * we cant change its value, we can use final in method and variable, classes.
+         * It is better to declare final variable in uppercase
+         **/
+
+
+        System.out.println("Name: "+NAME+" Age: "+AGE);
+
+
+    }
+}
