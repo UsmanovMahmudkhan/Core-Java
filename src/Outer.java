3bbaf8756be3aa5754c3b9fe2016a42e616635ed
mahmudkhonusmonov <usmanovmahmudkhan@gmail.com>
2025-07-31 15:37:54 +0900

    added inner/outer class

---
+++ src/Outer.java
@@ -0,0 +1,23 @@
+public class Outer {
+    int a = 10;
+
+    public void accessInner() {
+        Inner inner = new Inner();
+        inner.sayHello();
+        inner.b = 15;
+        System.out.println(inner.b);
+    }
+
+    class Inner {
+        int b = 5;
+        void sayHello() {
+            System.out.println("Hello from Inner");
+            System.out.println("Outer class variable a: " + a);
+        }
+    }
+
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+        outer.accessInner();
+    }
+}
