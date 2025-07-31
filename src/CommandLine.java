85783ef6309b3b47896ec9334d6cdc27bc94e5c1
mahmudkhonusmonov <usmanovmahmudkhan@gmail.com>
2025-08-01 01:31:52 +0900

    added CommandLine

---
+++ src/CommandLine.java
@@ -0,0 +1,19 @@
+public class CommandLine {
+
+    public static void main(String[] args) {
+        for(int i=0;i< args.length;i++){
+            System.out.println(args[i]);
+        }
+
+    }
+
+    /**
+     * mahmudkhonv@ src % javac CommandLine.java
+     * mahmudkhonv@ src % java CommandLine HElLO how are you aaaa
+     * HElLO
+     * how
+     * are
+     * you
+     * aaaa
+     */
+}
