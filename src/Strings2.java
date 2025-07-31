ec6e4b3aca93450f13820a3630570a755c408121
mahmudkhonusmonov <usmanovmahmudkhan@gmail.com>
2025-07-31 18:07:22 +0900

    added inner/outer2 class

--- src/Strings2.java
+++ src/Strings2.java
@@ -1,11 +1,24 @@
 public class Strings2 {
     public static void main(String[] args) {
         String myStr="my first String";
-        String mtStr2="my "+"second "+" String";
-        String myStr3;
+        String myStr2="my "+"second "+" String";
+        String myStr3=myStr+" <<<< this is actually third String object";
         System.out.println(myStr);
 
         char character=myStr.charAt(myStr.length()-1);
+        System.out.println(character);
+        int len=myStr.length();
+        System.out.println(len);
+
+        System.out.println(myStr2);
+
+        System.out.println(myStr3);
+
+
+
+
+
+
 
 
     }
