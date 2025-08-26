d8af482456aee3ff3b99f8eb3eaa69a7e5ac556e
mahmudkhonusmonov <usmanovmahmudkhan@gmail.com>
2025-08-26 16:22:07 +0900

    added Thread exercise

--- src/Basic_problems/BankThread.java
+++ src/Basic_problems/BankThread.java
@@ -1,13 +1,45 @@
 package Basic_problems;
 
-/**
- * @author mahmudkhonusmonov
- * Implement a bank account simulation.
- * Multiple threads should deposit and withdraw money from a shared account.
- * Use synchronized to avoid race conditions.
- */
 public class BankThread {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        BankAccount bank = new BankAccount();
 
+        Thread t1 = new Thread(bank, "T1");
+        Thread t2 = new Thread(bank, "T2");
+        Thread t3 = new Thread(bank, "T3");
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+        t1.join();
+        t2.join();
+        t3.join();
+
+        System.out.println("Final Balance: " + bank.getBalance());
+    }
+}
+
+class BankAccount implements Runnable {
+    private long balance = 100_000;
+
+    @Override
+    public void run() {
+        deposit(2000);
+        withdraw(20);
+    }
+
+    public synchronized void deposit(long amount) {
+        balance += amount;
+        System.out.println(Thread.currentThread().getName() + " deposited: +" + amount + " | Balance: " + balance);
+    }
+
+    public synchronized void withdraw(long amount) {
+        balance -= amount;
+        System.out.println(Thread.currentThread().getName() + " withdrew: -" + amount + " | Balance: " + balance);
+    }
+
+    public synchronized long getBalance() {
+        return balance;
     }
 }
