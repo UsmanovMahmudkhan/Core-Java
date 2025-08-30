81ebebbad082ab005200358ece4915cd5f1c4574
mahmudkhonusmonov <usmanovmahmudkhan@gmail.com>
2025-08-31 00:17:52 +0900

    add wrapper class concept

---
+++ src/Basic_problems/BasicAutoUnboxing.java
@@ -0,0 +1,10 @@
+package Basic_problems;
+
+public class BasicAutoUnboxing {
+    public static void main(String[] args) {
+        float number_prim=12.4F;
+        Float Wnum=number_prim;
+        System.out.println(Wnum);
+        System.out.println(number_prim);
+    }
+}
