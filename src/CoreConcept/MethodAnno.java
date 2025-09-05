3deec640235e18f55af2bbc6e9e381567b163939
mahmudkhonusmonov <usmanovmahmudkhan@gmail.com>
2025-09-05 20:30:50 +0900

    playing with annotation on interface

---
+++ src/CoreConcept/MethodAnno.java
@@ -0,0 +1,49 @@
+package CoreConcept;
+
+import java.lang.Object;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+public class MethodAnno {
+    public static void main(String[] args) throws NoSuchMethodException {
+        FullAnno.getFull();
+    }
+}
+
+@Method_Annotation
+class FullAnno{
+
+    @Method_Annotation2(description = "salom",age=11)
+    public static void getFull() throws NoSuchMethodException {
+
+        Class<?>object= FullAnno.class;
+        System.out.println(object);
+
+        Method_Annotation methodAnnotation=object.getAnnotation(Method_Annotation.class);
+        System.out.println(methodAnnotation);
+
+        Method_Annotation2 methodAnnotation2=object.getMethod("getFull").getAnnotation(Method_Annotation2.class);
+
+        System.out.println(methodAnnotation2);
+
+
+    }
+
+}
+
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@interface Method_Annotation{
+    String description() default "this is for class";
+    int value() default 20;
+}
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@interface Method_Annotation2{
+    String description();
+    int age();
+}
