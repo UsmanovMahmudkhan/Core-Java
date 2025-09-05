06591b4895d414ef71cce78842dd7a0e394900ec
mahmudkhonusmonov <usmanovmahmudkhan@gmail.com>
2025-09-05 18:29:13 +0900

    playing with annotation on interface

---
+++ src/Leetcode/Sequence.java
@@ -0,0 +1,19 @@
+package Leetcode;
+
+public class Sequence {
+
+    public static void main(String[] args) {
+        System.out.println(isSubsequence("salom","assalomhasyt"));
+    }
+     public static boolean isSubsequence(String s, String t) {
+            int i = 0, j = 0;
+            while(i < s.length() && j < t.length()){
+                if(s.charAt(i) == t.charAt(j)){
+                    i++;
+                }
+                j++;
+            }
+            return i == s.length();
+        }
+    }
+
