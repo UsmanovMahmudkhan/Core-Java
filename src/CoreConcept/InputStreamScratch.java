96ae14ba889ff4ea5d3e0f8e68ba5bf4d227af34
mahmudkhonusmonov <usmanovmahmudkhan@gmail.com>
2025-09-09 00:55:23 +0900

    exercising with bytes

---
+++ src/CoreConcept/InputStreamScratch.java
@@ -0,0 +1,15 @@
+package CoreConcept;
+
+import java.io.IOException;
+
+public class InputStreamScratch {
+    public static void main(String[] args) throws Exception {
+
+        int data=System.in.read();
+        System.out.write(data);
+        System.out.flush();
+
+
+
+    }
+}
