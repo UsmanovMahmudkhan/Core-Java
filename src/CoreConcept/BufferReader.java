7a2fbd8fd7da352c91a7d176c8f2c8da1b07e7a7
mahmudkhonusmonov <usmanovmahmudkhan@gmail.com>
2025-09-11 00:53:22 +0900

    exercising with bytes

--- src/CoreConcept/BufferReader.java
+++ src/CoreConcept/BufferReader.java
@@ -9,8 +9,13 @@ class BufferReaderTest {
     public static void main(String[] args) throws Exception {
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in,System.console().charset()));
         int inp=br.read();
-        br.close();
         System.out.println((char)inp);
+        BufferedInputStream br1= new BufferedInputStream(System.in);
+        InputStreamReader my=new InputStreamReader(br1);
+        int data=my.read();
+        System.out.println((char)data);
+        br.close();
+        my.close();
 
     }
 }
