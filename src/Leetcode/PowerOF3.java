46caf8bf9b1d75af07fd55ca1d63f4ae0d2238bf
mahmudkhonusmonov <usmanovmahmudkhan@gmail.com>
2025-09-13 01:38:37 +0900

    messing up with leetcode

---
+++ src/Leetcode/PowerOF3.java
@@ -0,0 +1,29 @@
+package Leetcode;
+
+/**
+ * I know it is not that good code, but at least done by me and will be refactored by me, NOT AI
+ */
+class PowerOfThree {
+    public static void main(String[] args) {
+        System.out.println( isPowerOfThree(27));
+    }
+    public static  boolean isPowerOfThree(int n) {
+
+        boolean isPower=false;
+
+        if(n==1){
+            isPower=true;
+        }
+
+        while(n%3==0 && n!=0){
+            if(n==3){
+                isPower=true;
+            }
+
+            n/=3;
+
+        }
+
+        return isPower;
+    }
+}
