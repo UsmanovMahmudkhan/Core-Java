a832ef09e129069d0e69a19deae8651c32ce6d56
mahmudkhonusmonov <usmanovmahmudkhan@gmail.com>
2025-10-03 00:42:35 +0900

    another generic exer_3

--- src/Basic_problems/Tuple.java
+++ src/Basic_problems/Tuple.java
@@ -9,11 +9,41 @@ package Basic_problems;
  - swap(new Tuple<>(1, "apple"))  → ("apple", 1)
  - combine(new Tuple<>(1, "A"), new Tuple<>(2, "B")) → ([1, 2], [A, B])
  */
-public class Tuple {
-    public static void main(String[] args) {
+import java.util.List;
 
+public class Tuple<A, B> {
+    private final A first;
+    private final B second;
+
+    public Tuple(A first, B second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public A getFirst() {
+        return first;
+    }
+
+    public B getSecond() {
+        return second;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + first + ", " + second + ")";
+    }
+
+    public static <X, Y> Tuple<Y, X> swap(Tuple<X, Y> tuple) {
+        return new Tuple<>(tuple.getSecond(), tuple.getFirst());
+    }
+
+    public static <X, Y> Tuple<List<X>, List<Y>> combine(Tuple<X, Y> t1, Tuple<X, Y> t2) {
+        return new Tuple<>(List.of(t1.getFirst(), t2.getFirst()), List.of(t1.getSecond(), t2.getSecond()));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Tuple.swap(new Tuple<>(1, "apple")));
+        System.out.println(Tuple.combine(new Tuple<>(1, "A"), new Tuple<>(2, "B")));
     }
 }
-
-
 
