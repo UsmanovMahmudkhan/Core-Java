d18b1a0d59515f478c53e982740ba1af49802956
mahmudkhonusmonov <usmanovmahmudkhan@gmail.com>
2025-10-05 01:47:37 +0900

    another gen exer_8

---
+++ src/Basic_problems/GenStack.java
@@ -0,0 +1,40 @@
+package Basic_problems;
+
+import java.util.Arrays;
+
+/**
+ * Implement a class GenericStack<T> with methods push, pop, and peek.
+ */
+public class GenStack {
+    public static void main(String[] args) {
+        GenericStack<Integer>gen= new GenericStack<>(new Integer[]{1,2,3,4});
+        System.out.println(Arrays.toString(gen.values));
+        System.out.println(Arrays.toString(gen.push(12)));
+        System.out.println(Arrays.toString(gen.push(15)));
+    }
+}
+
+class GenericStack<T>{
+    T [] values;
+
+    public GenericStack(T [] value) {
+        this.values = value;
+    }
+    public T [] getValues() {
+        return values;
+    }
+
+    public T[] push(T oneVal){
+        T[] newValues = (T[]) new Object[values.length+1];
+        newValues[0]=oneVal;
+        for(int i=0;i< values.length;i++){
+            newValues[i+1]=values[i];
+        }
+        values=newValues;
+
+        return values;
+    }
+
+
+
+}
