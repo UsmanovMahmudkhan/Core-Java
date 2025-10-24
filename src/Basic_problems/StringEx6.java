a4de0a1c07c3997d4eaa695f5bfbd28795d2adbf
mahmudkhonusmonov <usmanovmahmudkhan@gmail.com>
2025-10-24 23:57:34 +0900

    Generic Transformer

---
+++ src/Basic_problems/StringEx6.java
@@ -0,0 +1,12 @@
+package Basic_problems;
+
+//Concatenate a string with an integer and float value.
+
+public class StringEx6 {
+    public static void main(String[] args) {
+        String name="khan "+111;
+        String username="xan "+2.23F;
+        System.out.println(name+" "+username);
+    }
+
+}
