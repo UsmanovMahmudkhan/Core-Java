73441b5208bb2ee07dc4b10ac78fbbd050803d30
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2025-11-03 16:30:17 +0900

    StringEx17LowerUpper

---
+++ src/Basic_problems/StndInOut.java
@@ -0,0 +1,16 @@
+package Basic_problems;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class StinOut {
+    public static void main(String args[] ) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n= Integer.parseInt(br.readLine());
+
+        String name = br.readLine();
+        System.out.println(n*2);
+        System.out.println("Hi, " + name + ".");
+    }
+}
